71a7de5f2bcc7566b65f49eba9d913f78bcc3141
Bryce Krah <krahbryce@yahoo.com>
2016-02-02 17:59:50 -0500

    got timer working, starts at 45 and clears once it hits 0

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
+var $timerId = $('#timer');
+var seconds = 45;
+
+
+
+
+
+var game = {
+  start: function(){
+    var mytimer = setInterval(updateTime, 1000);
+
+    function updateTime(){
+      seconds--;
+      $($timerId).text("Shot Clock: " + seconds);
+      console.log(mytimer);
+      if(seconds === 0) {
+        clearInterval(mytimer);
+      }; // end if clearInterval
+    }; // end updateTime
+ } // end start
+
+
+
+
+};
+
+
+
+
+
 $( function() {
 //  alert("jquery loaded");
 
 
+game.start()
 
 
 
