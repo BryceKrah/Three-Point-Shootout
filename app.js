22810e1c8c05610ce29c6d09d36601a53a202b52
Bryce Krah <krahbryce@yahoo.com>
2016-02-02 18:24:32 -0500

    upon clicking start button, shot clock, player 1 and 2 score, and picture of basketball show up

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 var $timerId = $('#timer');
 var seconds = 45;
+var $playerOneScore = $('#first-score');
+var $playerTwoScore = $('#second-score');
+var $startButton = $('#btn');
+var $ball = $('#ball');
 
+var ball = {
+  made: false,
+  missed: false,
+  checkforMake: function(){
 
+  }
+}
 
 
 
 var game = {
+  playerOne: 0,
+  playerTwo: 0,
+
+
   start: function(){
+    $($playerOneScore).text("Player One Score: " + game.playerOne);
+    $($playerTwoScore).text("Player Two Score: " + game.playerTwo);
+    $($ball).append('<img class="basketball" src="http://icons.iconseeker.com/png/fullsize/nx10/basketball.png">')
+
     var mytimer = setInterval(updateTime, 1000);
 
     function updateTime(){
       seconds--;
       $($timerId).text("Shot Clock: " + seconds);
-      console.log(mytimer);
       if(seconds === 0) {
         clearInterval(mytimer);
       }; // end if clearInterval
     }; // end updateTime
+
+
+
  } // end start
 
 
@@ -32,7 +52,7 @@ $( function() {
 //  alert("jquery loaded");
 
 
-game.start()
+$($startButton).on('click', game.start);
 
 
 
