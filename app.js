688e24fa3feb20eb051c788c4e37acae9b8d204c
Bryce Krah <krahbryce@yahoo.com>
2016-02-02 19:31:56 -0500

    created function that will keep track of time click is held down, looked it up on stackoverflow, address is in js file

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ var $playerTwoScore = $('#second-score');
 var $startButton = $('#btn');
 var $ball = $('#ball');
 
+
+// http://stackoverflow.com/questions/1038677/how-can-i-measure-the-time-between-click-and-release-in-javascript
+// the begin function starts counting when the mouse is clicked logging its time down in miliseconds
+// the end function then subtracts the start time from the current time giving you the time down held down in miliseconds
+var startTime;
+function begin(){
+  startTime = new Date();
+};
+function end(){
+  var now = new Date();
+  console.log(now-startTime);
+  return (now -startTime);
+};
+
+
 var ball = {
   made: false,
   missed: false,
@@ -23,8 +38,7 @@ var game = {
   start: function(){
     $($playerOneScore).text("Player One Score: " + game.playerOne);
     $($playerTwoScore).text("Player Two Score: " + game.playerTwo);
-    $($ball).append('<img class="basketball" src="http://icons.iconseeker.com/png/fullsize/nx10/basketball.png">')
-
+    $($ball).append('<img class="basketball" src="http://icons.iconseeker.com/png/fullsize/nx10/basketball.png">');
     var mytimer = setInterval(updateTime, 1000);
 
     function updateTime(){
@@ -34,11 +48,18 @@ var game = {
         clearInterval(mytimer);
       }; // end if clearInterval
     }; // end updateTime
+    game.addEventToBall();
+ }, // end start
+
+ addEventToBall: function(){
+   $($ball).mousedown(begin);
+   $($ball).mouseup(end); //see above notes
+ },
+
+ shoot: function(){
 
 
-
- } // end start
-
+ }
 
 
 
