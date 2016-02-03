5576a892d3630a52d3f9e7a27cf425cbd8adb90a
Bryce Krah <krahbryce@yahoo.com>
2016-02-03 10:43:17 -0500

    score updates in real time, shot clock now starts when the ball is clicked

--- js/app.js
+++ js/app.js
@@ -1,73 +1,105 @@
 var $timerId = $('#timer');
-var seconds = 45;
+var seconds = 25;
 var $playerOneScore = $('#first-score');
 var $playerTwoScore = $('#second-score');
 var $startButton = $('#btn');
 var $ball = $('#ball');
-var startTime;
+
+
 // http://stackoverflow.com/questions/1038677/how-can-i-measure-the-time-between-click-and-release-in-javascript
 // the begin function starts counting when the mouse is clicked logging its time down in miliseconds
 // the end function then subtracts the start time from the current time giving you the time down held down in miliseconds
-
-
-
-var startTime;
-function begin(){
-  startTime = new Date();
-};
-function end(){
-  var now = new Date();
-  //console.log(now-startTime);
-  console.log(now -startTime);
-};
+//
+// var startTime;
+// function begin(){
+//   startTime = new Date();
+// };
+// function end(){
+//   var now = new Date();
+//   //console.log(now-startTime);
+//   console.log(now -startTime);
+// };
 
 
 var ball = {
   made: false,
   missed: false,
-  checkforMake: function(){
 
-  }
-}
+};
 
 
 
 var game = {
   playerOne: 0,
   playerTwo: 0,
+  scoreCount: 0,
 
 
   start: function(){
     $($playerOneScore).text("Player One Score: " + game.playerOne);
     $($playerTwoScore).text("Player Two Score: " + game.playerTwo);
     $($ball).append('<img class="basketball" src="http://icons.iconseeker.com/png/fullsize/nx10/basketball.png">');
-    var mytimer = setInterval(updateTime, 1000);
-
-    function updateTime(){
-      seconds--;
-      $($timerId).text("Shot Clock: " + seconds);
-      if(seconds === 0) {
-        clearInterval(mytimer);
-      }; // end if clearInterval
-    }; // end updateTime
-    game.addEventToBall();
+    $($timerId).text("Shot Clock: " + seconds);
+    // var mytimer = setInterval(updateTime, 1000);
+    //
+    // function updateTime(){
+    //   seconds--;
+    //   $($timerId).text("Shot Clock: " + seconds);
+    //   if(seconds === 0) {
+    //     clearInterval(mytimer);
+    //   }; // end if clearInterval
+    // }; // end updateTime
+    // game.addEventToBall();
  }, // end start
 
- addEventToBall: function(){
-   $($ball).mousedown(begin);
-   $($ball).mouseup(end); //see above notes
 
-  //  if ( > 1000 &&  < 2000){
-  //    console.log("thats a made shot");
-  //  };
+ updateScore: function(){
+   if (seconds !== 0) {
+   $($playerOneScore).text("Player One Score: " + game.playerOne);
+  };
  },
+
+ addEventToBall: function(){
+   var startTime;
+   function begin(){
+     startTime = new Date();
+   };
+   function end(){
+     var now = new Date();
+     //console.log(now-startTime);
+     var downTime = (now -startTime);
+     if (downTime > 1000 && downTime < 2000){
+       console.log("you made the shot");
+       game.scoreCount++
+       console.log(game.scoreCount);
+       game.playerOne = game.scoreCount;
+       game.updateScore();
+     } else {
+       console.log("you missed");
+     };
+   };
+   $($ball).mousedown(begin);
+   $($ball).mouseup(end) //see above notes
+
+ },
+
+ startTheTimer: function(){
+   $($ball).off('click');
+   var mytimer = setInterval(updateTime, 1000);
+   function updateTime(){
+     seconds--;
+     $($timerId).text("Shot Clock: " + seconds);
+     if(seconds === 0) {
+       clearInterval(mytimer);
+     }; // end if clearInterval
+   }; // end updateTime
+   game.addEventToBall();
+ },
+
 
  shoot: function(){
 
-
-
  }
-
 
 
 };
@@ -81,7 +113,7 @@ $( function() {
 
 
 $($startButton).on('click', game.start);
-
+$($ball).on('click', game.startTheTimer);
 
 
 });
